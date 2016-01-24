d6fa07b528361530d6b2cec43dc0d8ed2245b3ba
Kaarel Kaljurand <kaljurand@gmail.com>
2016-01-24 13:23:09 +0100

    Catch AttributeError when parsing TEI files

--- morph-analyze.py
+++ morph-analyze.py
@@ -12,6 +12,31 @@ from __future__ import print_function
 import argparse
 from estnltk import Text
 from estnltk.teicorpus import parse_tei_corpus
+import sys
+
+def get_target(fnm):
+    """
+    See: https://github.com/estnltk/estnltk/issues/42#issuecomment-172461822
+    """
+    if 'drtood' in fnm:
+        return 'dissertatsioon'
+    if 'ilukirjandus' in fnm:
+        return 'tervikteos'
+    if 'seadused' in fnm:
+        return 'seadus'
+    if 'EestiArst' in fnm:
+        return 'ajakirjanumber'
+    if 'foorum' in fnm:
+        return 'teema'
+    if 'kommentaarid' in fnm:
+        return 'kommentaarid'
+    if 'uudisgrupid' in fnm:
+        return 'uudisgrupi_salvestus'
+    if 'jututoad' in fnm:
+        return 'jututoavestlus'
+    if 'stenogrammid' in fnm:
+        return 'stenogramm'
+    return 'artikkel'
 
 def fix(el):
     if len(el):
@@ -35,15 +60,18 @@ def analyze_files(fns):
 
 def analyze_tei_files(fns):
     for fn in fns:
-        for text in parse_tei_corpus(fn, target=['artikkel']):
-            text['guess'] = True
-            text['disambiguate'] = True
-            show_df(text.get.word_texts.roots.postags.forms.as_dataframe)
+        try:
+            for text in parse_tei_corpus(fn, target=get_target(fn)):
+                text['guess'] = True
+                text['disambiguate'] = True
+                show_df(text.get.word_texts.roots.postags.forms.as_dataframe)
+        except AttributeError:
+            print('Warning: parse error: skipped: {0}'.format(fn), file=sys.stderr)
 
 def get_args():
     p = argparse.ArgumentParser(description='')
     p.add_argument('fns', metavar='N', type=str, nargs='+', help='')
-    p.add_argument('-v', '--version', action='version', version='%(prog)s v0.0.1')
+    p.add_argument('-v', '--version', action='version', version='%(prog)s v0.0.2')
     return p.parse_args()
 
 def main():
